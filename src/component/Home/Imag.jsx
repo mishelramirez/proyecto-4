2321b574547a7b8badad58dd0a4b7a1572932ec1
cintya <rmishel564@gmail.com>
2023-04-05 11:58:41 -0500

    se hacen modificaciones en estilos

--- src/component/Home/Imag.jsx
+++ src/component/Home/Imag.jsx
@@ -2,7 +2,7 @@ import style from "./home.css";
 import foto from "../../assets/hero-desktop.jpg"
 
 function Imag() {
-    return <div className="container">
+    return <div className="containerimg">
         
    <img src={foto} className="imag"/>
     </div>;
